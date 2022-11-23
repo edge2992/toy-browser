57eb219ca45685cf5d7d85db30dfd51782c420a1
edge2992 <torune.summer@gmail.com>
2022-11-23 23:58:32 +0900

    refactor: jscontextの型定義

--- src/jscontext.py
+++ src/jscontext.py
@@ -1,13 +1,18 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Dict, List, Union
 import dukpy
 from src.cssparser import CSSParser
-from src.text import HTMLParser
+from src.text import Element, HTMLParser
 from src.util.node import tree_to_list
 
-EVENT_DISPATCH_CODE = "new Node(dukpy.handle).dispatchEvent(new Event(dukpy.type))"
+if TYPE_CHECKING:
+    from src.graphics import Tab
+
+EVENT_DISPATCH_CODE: str = "new Node(dukpy.handle).dispatchEvent(new Event(dukpy.type))"
 
 
 class JSContext:
-    def __init__(self, tab):
+    def __init__(self, tab: Tab):
         self.tab = tab
         self.interp = dukpy.JSInterpreter()
         self.interp.export_function("log", print)
@@ -18,13 +23,13 @@ class JSContext:
         with open("src/runtime.js") as f:
             self.interp.evaljs(f.read())
 
-        self.node_to_handle = {}
-        self.handle_to_node = {}
+        self.node_to_handle: Dict[Element, int] = {}
+        self.handle_to_node: Dict[int, Element] = {}
 
     def run(self, code: str):
         return self.interp.evaljs(code)
 
-    def innerHTML_set(self, handle, s):
+    def innerHTML_set(self, handle: int, s: str):
         doc = HTMLParser("<html><body>" + s + "</body></html>").parse()
         new_nodes = doc.children[0].children
 
@@ -34,12 +39,12 @@ class JSContext:
             child.parent = elt
         self.tab.render()
 
-    def dispatch_event(self, type, elt):
+    def dispatch_event(self, type: str, elt: Element) -> bool:
         handle = self.node_to_handle.get(elt, -1)
         do_default = self.interp.evaljs(EVENT_DISPATCH_CODE, type=type, handle=handle)
         return not do_default
 
-    def get_handle(self, elt):
+    def get_handle(self, elt: Element) -> int:
         if elt not in self.node_to_handle:
             handle = len(self.node_to_handle)
             self.node_to_handle[elt] = handle
@@ -48,13 +53,13 @@ class JSContext:
             handle = self.node_to_handle[elt]
         return handle
 
-    def querySelectorAll(self, selector_text: str):
+    def querySelectorAll(self, selector_text: str) -> List[int]:
         selector = CSSParser(selector_text).selector()
-        nodes = [
+        nodes: List[Element] = [
             node for node in tree_to_list(self.tab.nodes, []) if selector.matches(node)
         ]
         return [self.get_handle(node) for node in nodes]
 
-    def getAttribute(self, handle, attr):
+    def getAttribute(self, handle: int, attr: str) -> Union[str, None]:
         elt = self.handle_to_node[handle]
         return elt.attributes.get(attr, None)
