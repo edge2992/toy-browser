cf054677890d4688bc1598d363983e56f717a1f3
edge2992 <torune.summer@gmail.com>
2022-11-26 18:03:24 +0900

    Drawクラスがrectを持つように修正

--- src/draw.py
+++ src/draw.py
@@ -5,36 +5,44 @@ from src.util.draw_skia import draw_line, draw_rect, draw_text, linespace, parse
 
 class Draw:
     def __init__(
-        self, top: float, left: float, right: float, bottom: float, color: str
+        self,
+        top: float,
+        left: float,
+        right: float,
+        bottom: float,
+        rect: skia.Rect,
+        color: str,
     ):
         self.top: float = top
         self.left: float = left
         self.right: float = right
         self.bottom: float = bottom
+        self.rect: skia.Rect = rect
         self.color: str = color
 
-    def execute(self, scroll: float, canvas):
+    def execute(self, canvas):
         raise NotImplementedError
 
 
 class DrawText(Draw):
     def __init__(self, x1: float, y1: float, text: str, font: skia.Font, color: str):
+        bottom = y1 + linespace(font)
         super().__init__(
             y1,
             x1,
             x1,
-            y1 + linespace(font),
+            bottom,
+            skia.Rect.MakeLTRB(x1, y1, x1, bottom),
             color,
         )
-        self.rect = skia.Rect.MakeLTRB(x1, y1, self.right, self.bottom)
         self.text = text
         self.font = font
 
-    def execute(self, scroll, canvas):
+    def execute(self, canvas):
         draw_text(
             canvas,
             self.left,
-            self.top - scroll,
+            self.top,
             self.text,
             self.font,
             self.color,
@@ -46,16 +54,15 @@ class DrawText(Draw):
 
 class DrawRect(Draw):
     def __init__(self, x1: float, y1: float, x2: float, y2: float, color: str):
-        super().__init__(y1, x1, x2, y2, color)
-        self.rect = skia.Rect.MakeLTRB(x1, y1, x2, y2)
+        super().__init__(y1, x1, x2, y2, skia.Rect.MakeLTRB(x1, y1, x2, y2), color)
 
-    def execute(self, scroll, canvas):
+    def execute(self, canvas):
         draw_rect(
             canvas,
             self.left,
-            self.top - scroll,
+            self.top,
             self.right,
-            self.bottom - scroll,
+            self.bottom,
             fill=self.color,
         )
 
@@ -65,10 +72,9 @@ class DrawRect(Draw):
 
 class DrawLine(Draw):
     def __init__(self, x1: float, y1: float, x2: float, y2: float):
-        super().__init__(y1, x1, x2, y2, "black")
-        self.rect = skia.Rect.MakeLTRB(x1, y1, x2, y2)
+        super().__init__(y1, x1, x2, y2, skia.Rect.MakeLTRB(x1, y1, x2, y2), "black")
 
-    def execute(self, scroll, canvas):
+    def execute(self, canvas):
         draw_line(
             canvas,
             self.left,
@@ -83,14 +89,13 @@ class DrawLine(Draw):
 
 class DrawRRect(Draw):
     def __init__(self, rect: skia.Rect, radius, color):
-        super().__init__(rect.top(), rect.left(), rect.right(), rect.bottom(), color)
         self.rect = rect
         self.rrect = skia.RRect.MakeRectXY(rect, radius, radius)
         self.color = color
 
-    def execute(self, scroll, canvas):
+    def execute(self, canvas):
         sk_color = parse_color(self.color)
         canvas.drawRRect(self.rrect, paint=skia.Paint(Color=sk_color))
 
     def __repr__(self) -> str:
-        return f"DrawRRect(top={self.top}, left={self.left}, right={self.right}, bottom={self.bottom}, color={self.color})"
+        return f"DrawRRect({self.color})"
